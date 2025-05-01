fae21edf4e03b501b499923d72f3ab2e42e3ad2b
fernandzxc <alifernandopinto@gmail.com>
2025-04-30 23:33:01 -0400

    actualizacion

--- src/components/Section_hero.jsx
+++ src/components/Section_hero.jsx
@@ -1,6 +1,7 @@
+import main from "/bg-masthead.jpg"
 const Section_hero = () => {
   return (
-    <div className="bg-[url('bg-masthead.jpg')] h-[calc(100vh-56px)] bg-cover object-contain bg-center flex flex-col justify-center items-center gap-16">
+    <div className="h-[calc(100vh-56px)] bg-cover object-contain bg-center flex flex-col justify-center items-center gap-16" style={{backgroundImage: `url(${main})`}}>
       <h1 className="text-teal-900 text-6xl w-[550px] text-center">
         Genera mas clientes potenciales con una pagina!
       </h1>
